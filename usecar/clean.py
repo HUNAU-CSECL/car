6d4a6a26efaa4d6e5197d60c5117c19a7e236927
sssnowyue <sssnowyue@163.com>
2017-07-23 23:01:08 +0800

    else founctions

--- usecar/clean.py
+++ usecar/clean.py
@@ -16,7 +16,7 @@ def clean(obj, count):
         dic1['aplace'] = a.aplace
         dic1['bplace'] = a.bplace
         dic1['start'] = time.mktime(a.start.timetuple())
-        dic1['ab_end'] = a.ab_end
+        dic1['ab_end'] = time.mktime(a.start.timetuple())-time.mktime(a.ab_end.timetuple())
         dic1['reason'] = a.reason
         dic1['att1'] = a.exam.att1
         dic1['att2'] = a.exam.att2
@@ -36,6 +36,31 @@ def clean(obj, count):
             dic1['end'] = time.mktime(a.end.timetuple())
         else:
             dic1['end'] = None
+        dic0[i] = dic1
+    dic0['count'] = int(count)
+    return json.dumps(dic0)
+
+def cars_info(obj,count):
+    dic0={}
+    i=0
+    for a in obj:
+        dic1={}
+        i += 1
+        dic1['lic']=a.lic
+        dic1['style']=a.brand+a.style
+        dic1['cap']=a.cap
+        dic0[i] = dic1
+    dic0['count'] = int(count)
+    return json.dumps(dic0)
+
+def drivers_info(obj,count):
+    dic0={}
+    i=0
+    for a in obj:
+        dic1={}
+        i += 1
+        dic1['name']=a.name
+        dic1['tel']=a.tel
         dic0[i] = dic1
     dic0['count'] = int(count)
     return json.dumps(dic0)
--- usecar/views.py
+++ usecar/views.py
@@ -18,27 +18,7 @@ from .clean import *
 
 
 def test(request):
-    start='1500807262'
-    ab_end='2678400'
-    end=int(start)+int(ab_end)
-    start_data=datetime.datetime.fromtimestamp(int(start))
-    end_data=datetime.datetime.fromtimestamp(int(end))
-    obj=models.Application.objects.filter(Q(end__isnull=False),Q(start__range=(start_data,end_data))|Q(ab_end__range=(start_data,end_data))|Q(start__lt=start_data,ab_end__gt=end_data))
-    per_obj=models.Persons.objects.filter(role=1)
-    car_obj=models.Cars.objects.all()
-    dic={}
-    dic1={}
-    dic2={}
-    for per in per_obj:
-        dic1[str(per.id)]={'name':per.name,'tel':per.tel,'state':'1'}
-    for car in car_obj:
-        dic2[str(car.id)]={'style':car.brand+car.style,'lic':car.license,'cap':car.cap,'state':'1'}
-    for a in obj:
-        dic1[str(a.driver.id)]['state']='0'
-        dic2[str(a.car.id)]['state']='0'
-    dic['drivers']=dic1
-    dic['cars']=dic2
-    return render(request, 'usecar/test.html', {'response': dic})
+    return render(request, 'usecar/test.html', {'response': 1})
 
 
 def login(request):
@@ -375,4 +355,75 @@ def distributed(request):
         '-id')[int(amount):int(amount) + int(last)]
     json_str = clean(obj, count)
     return HttpResponse(json_str)
-    
+
+#待审核
+def to_check(request):
+    last = request.POST.get('last')
+    amount = request.POST.get('amount')
+    role=request.get.session('role')
+    per_obj = models.Persons.objects.get(id=request.get.session('per_id'))
+    if role==4:
+        count = models.Exam.objects.filter(exam1=per_obj, att1__isnull=True).count()
+        obj = models.Application.objects.filter(exam__exam1=per_obj, exam__att1__isnull=True).order_by('-id')[int(amount):int(amount) + int(last)]
+        json_str = clean(obj, count)
+    else:
+        count = models.Exam.objects.filter(exam2=per_obj, att2__isnull=True,att1__isnull=False).count()
+        obj = models.Application.objects.filter(exam__exam2=per_obj, exam__att2__isnull=True,att1__isnull=False).order_by('-id')[int(amount):int(amount) + int(last)]
+        json_str = clean(obj, count)
+    return HttpResponse(json_str)
+#审核
+def check(request):
+    appl_id=request.POST.get('id')
+    role=request.get.session('role')
+    att=int(request.POST.get('att'))
+    obj = models.Application.objects.get(id=appl_id)
+    if role==4:
+        obj.exam.att1 = att
+    else:
+        obj.exam.att2 = att
+    obj.exam.save()
+    return JsonResponse({'msg': ok})
+#审核成功
+def checked(request):
+    last = request.POST.get('last')
+    amount = request.POST.get('amount')
+    role=request.get.session('role')
+    per_obj = models.Persons.objects.get(id=request.get.session('per_id'))
+    if role==4:
+        count = models.Exam.objects.filter(exam1=per_obj, att1__isnull=False).count()
+        obj = models.Application.objects.filter(exam__exam1=per_obj, exam__att1__isnull=False).order_by('-id')[int(amount):int(amount) + int(last)]
+        json_str = clean(obj, count)
+    else:
+        count = models.Exam.objects.filter(exam2=per_obj, att2__isnull=False).count()
+        obj = models.Application.objects.filter(exam__exam2=per_obj, exam__att2__isnull=False).order_by('-id')[int(amount):int(amount) + int(last)]
+        json_str = clean(obj, count)
+    return HttpResponse(json_str)
+
+#车辆
+def allCars(request):
+    last = request.POST.get('last')
+    amount = request.POST.get('amount')
+    count=models.Cars.objects.all().count()
+    obj=models.Cars.objects.all().order_by('-id')[int(amount):int(amount) + int(last)]
+    json_str = cars_info(obj, count)
+    return HttpResponse(json_str)
+#司机
+def allDrivers(request):
+    last = request.POST.get('last')
+    amount = request.POST.get('amount')
+    count=models.Persons.objects.filter(role=1).count()
+    obj=models.Persons.objects.filter(role=1).order_by('-id')[int(amount):int(amount) + int(last)]
+    json_str=drivers_info(obj, count)
+    return HttpResponse(json_str)
+#添加车辆
+def inser_car(request):
+    obj=models.Companies.objects.get(id=request.session.get('com_id'))
+    u = models.Cars(license=request.POST.get('lic'), brand=request.POST.get('brand'), style=request.POST.get('style'),cap=request.POST.get('cap'),co=obj)
+    u.save()
+    return JsonResponse({'msg': ok})
+#添加司机
+def inser_driver(request):
+    obj=models.Companies.objects.get(id=request.session.get('com_id'))
+    u = models.Persons(name=request.POST.get('name'), tel=request.POST.get('tel'), role=1,co=obj)
+    u.save()
+    return JsonResponse({'msg': ok})
